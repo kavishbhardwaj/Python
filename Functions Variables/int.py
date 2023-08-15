004d0bc8644f9dd00ac23c88162b88b8a9966e15
kavishbhardwaj <kavishbhardwaj@gmail.com>
2023-08-15 16:36:44 +0530

    classification

---
+++ Functions Variables/int.py
@@ -0,0 +1,17 @@
+#This program introduces numerical data type "integer" and functions
+
+x = input("what's x ?")
+y = input("What's y ?")
+
+z = x+y
+print(z)
+
+z = int(x) + int(y)       # int is not only a data type but also a function
+print("No, the correct sum is", z)
+
+x = int(input("what's x again?"))  #nesting of functions
+y = int(input("What's y again?"))
+
+print(x+y)
+
+print(int(input("What's x ?"))+int(input("What's y ?")))      #nesting of functions
