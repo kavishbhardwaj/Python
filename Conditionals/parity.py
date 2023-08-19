76c98ed3a6437f64cd32c2d97e44a6cb5fca9eaa
kavishbhardwaj <kavishbhardwaj@gmail.com>
2023-08-19 16:03:57 +0530

    offline edits

---
+++ Conditionals/parity.py
@@ -0,0 +1,16 @@
+#Finding whether a number is even or odd without modulo operator
+#The modulo % operator in programming allows one to see if two numbers divide evenly or divide and have a remainder.
+
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+
+def is_even(n):
+    return True if n % 2 == 0 else Flase      #Pythonic way
+    # return n % 2 == 0                       #Alternate way to perfrom same function
+
+main()
