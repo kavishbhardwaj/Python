e724de23d9a46c1faa1f23c74ddf80f8135c8fd7
kavishbhardwaj <kavishbhardwaj@gmail.com>
2024-09-28 14:35:25 +0530

    week 2 ongoing - ipynb files restored 2.30pm

--- Conditionals and loops/loopwhile&control.py
+++ Conditionals and loops/loopwhile&control.py
@@ -1,3 +1,12 @@
+#minimum positive integer x such that (x**2 + x + 41) is divisible by 41
+
+z = 40
+x = 1
+while z % 41 != 0:
+    z = x**2 + x + 41
+    x = x + 1
+print(x-1)
+
 
 # prints all positive integers less than or equal to 50 that are divisible by 3
 x = 0
