7bab4268ccd73a98db2e20536dea5cfe7ddc07f7
Kavish Bhardwaj <kavishbhardwaj@gmail.com>
2024-12-05 12:08:47 +0530

    Removed C and Linux_shell folders

--- Linux_shell/random_toss.py
+++
@@ -1,9 +0,0 @@
-import random
-n = int(input("Enter number of tosses"))
-# n = 10
-heads = 0
-for i in range(n):
-    toss = random.choice('HT')
-    if toss == 'H':
-        heads += 1
-print(f'P(H) = {heads / n}')
